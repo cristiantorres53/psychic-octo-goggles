07ec883000a464ae9ed4039ce5fa1f75cceddf1d
Cristian Camilo Torres Rodriguez <47953810+cristiantorres53@users.noreply.github.com>
2022-03-08 09:55:37 -0500

    creacion de api

---
+++ script/users.js
@@ -0,0 +1,41 @@
+let formulario = document.getElementsByClassName('form-registro')
+let btnBuscar = document.getElementById('')
+let btnEliminar = document.getElementById('')
+let btnEditar = document.getElementById('')
+
+let url = 'https://ev2022.herokuapp.com/usuarios/'
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('id').style.display = 'none'
+    document.getElementById('label-edit').style.display = 'none'
+})
+
+formulario.addEventListener('submit', async e => {
+    e.preventDefault()
+
+    let nombre = document.getElementsByClassName('input-nombre').value
+    let apellido = document.getElementsByClassName('input-apellido').value
+    let fechaNacimiento = document.getElementsByClassName('input-fechaNacimiento').value
+    let cedula = document.getElementsByClassName('input-cedula').value
+    let correo = document.getElementsByClassName('input-correo').value
+    let pass = document.getElementsByClassName('input-contraseña').value
+
+    let resp = await fetch(url, {
+        method: 'POST',
+        body: JSON.stringify({
+            nombre: nombre,
+            apellido: apellido,
+            fechaNacimiento : fechaNacimiento,
+            cedula: cedula,
+            correo: correo,
+            pass:pass
+        }),
+        headers: {
+            "Content-Type": "application/json; charset=UTF-8"
+        }
+    })
+    document.getElementById("formulario").reset()
+})
+
+
+
